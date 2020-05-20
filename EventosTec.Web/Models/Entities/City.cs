41d98315896d92756036c768a4141e78320c8e9e
aldocuellar <60556844+aldocuellar@users.noreply.github.com>
2020-05-20 04:22:38 -0700

    Layout

---
+++ EventosTec.Web/Models/Entities/City.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventosTec.Web.Models.Entities
+{
+    public class City
+    {
+    }
+}
